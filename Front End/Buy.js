c039890d59e98e520dfe1bf7884bcfcd1191ce14
Michael Lee <leemic577@gmail.com>
2021-10-13 18:05:06 -0400

    json read

--- Front End/Buy.js
+++ Front End/Buy.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-  loadFunction();
+  makeRows(16, 16);
 
 };
 
@@ -7,13 +7,29 @@ window.onload = function() {
 
 
 
-function loadFunction() {
-    document.getElementById("test").innerHTML = "jdbhfb";
-      var data = JSON.parse(test);
-      document.getElementById("test").innerHTML = "nee";
-      document.getElementById("test").innerHTML = data.House;
-}
+//function loadFunction() {
+//  makeImageGrid(5);
 
-function filter() {
+//}
 
-}
+const container = document.getElementById("container");
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    cell.innerText = (c + 1);
+    container.appendChild(cell).className = "grid-item";
+  };
+};
+
+
+
+//function filter() {
+
+//}
+
+//function searchItems() {
+
+//}
