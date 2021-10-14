c11ef8fa400245897a8078c837c61566bb5ba793
Michael Lee <leemic577@gmail.com>
2021-10-13 22:58:49 -0400

    prototype 1

--- Front End/Buy.js
+++ Front End/Buy.js
@@ -1,35 +1,70 @@
-window.onload = function() {
-  makeRows(16, 16);
+class itemPreview {
+  constructor(albumId,id,title, url) {
+    this.albumId = albumId;
+    this.id = id;
+    this.title = title;
+    this.url;
+  }
+}
 
-};
+async function loadItems(num) {
+    var img = new itemPreview();
+
+    img.url= await (fetch('https://jsonplaceholder.typicode.com/photos')
+      .then(response => response.json())
+      .then(json => json[num].thumbnailUrl));
+
+    img.albumId= await (fetch('https://jsonplaceholder.typicode.com/photos')
+        .then(response => response.json())
+        .then(json => json[num].albumId));
+
+    img.id= await (fetch('https://jsonplaceholder.typicode.com/photos')
+        .then(response => response.json())
+        .then(json => json[num].id));
+
+    img.title= await (fetch('https://jsonplaceholder.typicode.com/photos')
+        .then(response => response.json())
+        .then(json => json[num].title));
 
 
+  return img;
+}
+
+async function createGrid() {
+  var galNum=1;
+  const gallery = "gallery";
+  for (let i = 0;i < 20;i++) {
+
+    var item = await loadItems(i);
+     var src = document.getElementById(gallery.concat("" + galNum));
+    if (galNum < 3) {
+      galNum++;
+    }
+    else {
+      galNum =1;
+    }
+    console.log(galNum);
+
+    var title = document.createElement("h5");
+    var textTitle = document.createTextNode(item.title)
+    title.appendChild(textTitle);
+    src.appendChild(title);
+
+    var img = document.createElement("img");
+    img.src = item.url;
+    src.appendChild(img);
+
+    var albumId = document.createElement("p");
+    var text = document.createTextNode(item.albumId)
+    albumId.appendChild(text);
+    src.appendChild(albumId);
+
+    var id = document.createElement("p");
+    var textId = document.createTextNode(item.id)
+    albumId.appendChild(textId);
+    src.appendChild(id);
 
 
-
-//function loadFunction() {
-//  makeImageGrid(5);
-
-//}
-
-const container = document.getElementById("container");
-
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
-  };
-};
-
-
-
-//function filter() {
-
-//}
-
-//function searchItems() {
-
-//}
+  }
+}
+createGrid();
