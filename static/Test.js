6e5cb2c2b2e1fc76f2c9f996508ec94967ec5fcc
Michael Lee <leemic577@gmail.com>
2021-10-15 05:34:36 -0400

    final?

--- static/Test.js
+++ static/Test.js
@@ -50,7 +50,7 @@ async function loadItems(num) {
 async function createGrid(callback) {
   var galNum=1;
   const gallery = "gallery";
-  for (let i = 0;i < 20;i++) {
+  for (let i = 0;i < jsonSize;i++) {
 
     var item = await loadItems(i);
     var src = document.getElementById(gallery.concat("" + galNum));
@@ -96,7 +96,7 @@ async function createGrid(callback) {
 
 async function searchTerms(item, searchTerms, galNum) {
   const gallery = "gallery";
-  if (item.title.includes(searchTerms)) {
+  if (item.name.includes(searchTerms)||item.useId.includes(searchTerms)||item.description.includes(searchTerms)) {
 
     var src = document.getElementById(gallery.concat("" + galNum));
     if (galNum < 3) {galNum++;}
@@ -115,10 +115,6 @@ async function searchTerms(item, searchTerms, galNum) {
     var textTitle = document.createTextNode(temp0.concat("Name: " , item.name));
     title.appendChild(textTitle);
     itemContainer.appendChild(title);
-
-    //var img = document.createElement("img");
-  //  img.src = item.url;
-  //  itemContainer.appendChild(img);
 
     var colour = document.createElement("p");
     var text = document.createTextNode(temp1.concat("\nColour: ", item.colour , "\n"));
@@ -143,8 +139,53 @@ async function searchTerms(item, searchTerms, galNum) {
     return galNum;
 }
 
+async function searchColours(item, colour, galNum) {
+  const gallery = "gallery";
+  if (colour.includes(item.colour)) {
+
+    var src = document.getElementById(gallery.concat("" + galNum));
+    if (galNum < 3) {galNum++;}
+    else {galNum =1;}
+    var temp0="";
+    var temp1="";
+    var temp2="";
+    var temp3="";
+    var temp4="";
+
+    var itemContainer = document.createElement("div");
+    itemContainer.style.cssText = "height:300px"
+    src.appendChild(itemContainer);
+
+    var title = document.createElement("h5");
+    var textTitle = document.createTextNode(temp0.concat("Name: " , item.name));
+    title.appendChild(textTitle);
+    itemContainer.appendChild(title);
+
+    var colour = document.createElement("p");
+    var text = document.createTextNode(temp1.concat("\nColour: ", item.colour , "\n"));
+    colour.appendChild(text);
+    itemContainer.appendChild(colour);
+
+    var description = document.createElement("p");
+    var textId = document.createTextNode(temp1.concat("Description: ", item.description , "\n"));
+    description.appendChild(textId);
+    itemContainer.appendChild(description);
+
+    var price = document.createElement("p");
+    var priceTag = document.createTextNode(temp3.concat("Price: " , item.price , "$\n"));
+    price.appendChild(priceTag);
+    itemContainer.appendChild(price);
+
+    var userId = document.createElement("p");
+    var userID = document.createTextNode(temp4.concat("Seller ID: ", item.userId));
+    userId.appendChild(userID);
+    itemContainer.appendChild(userId);
+    }
+    return galNum;
+}
+
+
 async function searchItems(terms, callback) {
-  jsonSize = getSize();
   document.getElementById("searchButton").disabled = true;
   document.getElementById("gallery1").innerHTML = "";
   document.getElementById("gallery2").innerHTML = "";
@@ -159,16 +200,38 @@ async function searchItems(terms, callback) {
   callback();
 }
 
-async function colourCheck(isRed, callback) {
+async function colourCheck(callback) {
   document.getElementById("searchButton").disabled = true;
   document.getElementById("gallery1").innerHTML = "";
   document.getElementById("gallery2").innerHTML = "";
   document.getElementById("gallery3").innerHTML = "";
 
+  var allColours = "";
+
+  if (document.getElementById('redCheck').checked){
+    allColours.concat("red");
+  }
+  if (document.getElementById('orangeCheck').checked){
+    allColours.concat("orange");
+  }
+  if (document.getElementById('yellowCheck').checked){
+    allColours.concat("yellow");
+  }
+  if (document.getElementById('greenCheck').checked){
+    allColours.concat("green");
+  }
+  if (document.getElementById('blueCheck').checked){
+    allColours.concat("blue");
+  }
+  if (document.getElementById('purpleCheck').checked){
+    allColours.concat("purple");
+  }
+
+
   var galNum=1;
   for (var i=0;i<jsonSize;i++) {
     var items = await loadItems(i);
-    galNum = searchTerms(items,terms,galNum);
+    galNum = searchColours(items,allColours,galNum);
   }
 
   callback();
